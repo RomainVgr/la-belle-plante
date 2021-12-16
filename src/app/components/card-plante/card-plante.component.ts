a8cf2e06686280280e1037eb1a8644723f711744
Romain <51331158+zylfu@users.noreply.github.com>
2021-12-16 20:38:32 +0100

    icons

--- src/app/components/card-plante/card-plante.component.ts
+++ src/app/components/card-plante/card-plante.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-plante',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-plante.component.scss']
 })
 export class CardPlanteComponent implements OnInit {
-
+  @Input() plant: any;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
