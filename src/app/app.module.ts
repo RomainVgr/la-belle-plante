9a291df154358713446844012521086f0e8f9975
Romain <51331158+zylfu@users.noreply.github.com>
2021-12-16 12:01:16 +0100

    4- filter-side-bar / integration card-plant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { PageAccueilComponent } from './pages/page-accueil/page-accueil.component';
 import { PageDetailsComponent } from './pages/page-details/page-details.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { FilterSideBarComponent } from './components/filter-side-bar/filter-side-bar.component';
+import { CardPlanteComponent } from './components/card-plante/card-plante.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
     NavBarComponent,
     PageAccueilComponent,
     PageDetailsComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    FilterSideBarComponent,
+    CardPlanteComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/page-accueil/page-accueil.component.ts
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { list_products } from 'src/app/data';
+import * as _ from 'underscore';
+
 
 @Component({
   selector: 'app-page-accueil',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-accueil.component.scss']
 })
 export class PageAccueilComponent implements OnInit {
+listdata = list_products  
+public listCategoriesFilter!: string[]
 
-  constructor() { }
+
+// evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+
+  constructor() {
+
+    this.listCategoriesFilter = []
+   }
 
   ngOnInit(): void {
-  }
+    console.log(this.listdata)
 
+    /**
+     * technique avec underscore.js pour récuperer les categories uniques
+     */
+    const lisAllCategories = this.listdata.map(product => product.product_breadcrumb_label)
+
+    const listUniqCategories = _.uniq(lisAllCategories)
+    console.log(listUniqCategories)
+    
+    /**
+     * technique native js pour récuperer les categories uniques
+     */
+
+const listUniqJSCategories = [...new Set(lisAllCategories)];
+console.log(listUniqJSCategories)
+
+this.listCategoriesFilter = listUniqCategories
+
+    
+  }
 }
+
+
+
+
+
